b6bf2223c9bfae4c68a1934f85ef53d88cf8037b
“kishlaychandan00” <kishlaychandan00@email.com>
2024-09-29 20:22:08 +0530

    updated cors origin

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 const corsOptions={
-    origin:"http://localhost:5173",
+    // origin:"http://localhost:5173",
+    origin:"https://ecommerce-two-jade.vercel.app/",
     credentials:true,
     method:["GET","POST","PUT","DELETE","OPTIONS","PATCH"],
     allowHeaders:["Authorization"],
