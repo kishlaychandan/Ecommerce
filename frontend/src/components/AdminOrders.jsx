63c4faadceaf47e7c0d1df93288e025c317d864a
“kishlaychandan00” <kishlaychandan00@email.com>
2024-11-08 14:55:33 +0530

    updated admin orders

--- frontend/src/components/AdminOrders.jsx
+++ frontend/src/components/AdminOrders.jsx
@@ -95,7 +95,7 @@ console.log("response", response);
   }
 
   return (
-    <div className="max-w-6xl mx-auto p-4">
+    <div className="max-w-5xl mx-auto p-3">
       <h1 className="text-3xl font-bold mb-6">All Orders (Admin)</h1>
 
       {/* Filter by status */}
@@ -122,7 +122,7 @@ console.log("response", response);
         orders.map((order) => (
           <div
             key={order._id}
-            className="border p-6 rounded-lg mb-6 shadow-lg bg-white transition-transform transform hover:scale-105"
+            className="border p-3 rounded-lg mb-6 shadow-lg bg-white transition-transform transform hover:scale-105"
           >
             <h2 className="text-2xl font-semibold">Order ID: <span className="text-[19px]">{order._id}</span></h2>
             <p>
