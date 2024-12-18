4e5f3eaa0758e214d684c0bd61cd2e9436acaaa7
“kishlaychandan00” <kishlaychandan00@email.com>
2024-12-18 23:39:10 +0530

    updated ShopNav.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -93,7 +93,7 @@ function Header() {
       className={`z-50 sticky top-0 p-5 w-full py-3 flex items-center justify-between transition-all duration-3000 ease-in-out ${
         isDarkMode ? "" : "bg-gray-100 text-[#db7b40]"
       }
-      ${scrolling ? (isDarkMode? "bg-gray-500 text-white py-6":"bg-slate-300 py-6") : (isDarkMode ? "bg-slate-500" : "bg-slate-200 ")}
+      ${scrolling ? (isDarkMode? "bg-gray-500 text-white py-6":"bg-slate-300 py-6") : (isDarkMode ? "bg-slate-500 text-white" : "bg-slate-200 ")}
       `}
     >
       {/* Left Side Logo */}
