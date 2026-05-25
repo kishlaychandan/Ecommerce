42b57df7ed5a1cbea25e7e28bcbb7ca872f93171
Kishlay Chandan <kishlaychandan00@gmail.com>
2026-05-25 11:55:42 +0530

    adding changes

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: "http://localhost:3000/api",
-    baseURL: "https://ecommerce-xsol.onrender.com/api",
+    baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
     withCredentials: true,
-
 });
 
 export default instance;
